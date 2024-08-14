0f07409f1d92acd1e6d5204eec747336673bd883
Pedrin0030 <pedro991@outlook.com.br>
2024-08-13 22:00:26 -0300

    primeiro exercicio de fundamentos

---
+++ Exercicios_JavaScript/exercicios1_fundamentos.js
@@ -0,0 +1,8 @@
+/*Crie uma função que retorne a string "Ólá" concatenada com um argumento text (a ser passado para a função)
+e com ponto de exclamação "!" no final
+*/
+function comprimentar(texto){
+    return console.log('Olá ' + texto + '!');
+}
+
+comprimentar("Roberto")
